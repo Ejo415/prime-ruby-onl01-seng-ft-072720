83734f48105efc53955e69a537102874895ac217
Eugene O'Neil <ejo7220@gmail.com>
2020-07-30 21:02:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,17 +1,7 @@
 
 
 def prime?(num)
- range = (2..(num - 1)).to_a check = true 
- if number < 2 
-   check = false 
- else
-   range.each do |i|
-     if num % i == 0 
-       check = false
-       break
-     end
-   end
- end
- puts check
+ Math.sqrt(num).floor.downto(2).each {|i| return false if num % i == 0}
+   true
 end 
 
